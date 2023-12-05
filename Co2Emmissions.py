c394c582192eba742419c06f9d1af5c0e3bc32ba
Nethra <nethragurumallesh08@gmail.com>
2023-12-05 11:06:09 +0000

    Uploading files for the working code

---
+++ Co2Emmissions.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import kurtosis, skew
+
+def plot_line(path_to_file):
+    """
+    Generate a line plot representing CO2 Emissions from 2000 to 2020 for different countries.
+
+    Parameters:
+    - path_to_file (str): The path to the CSV file containing the dataset.
+
+    Returns:
+    None
+    """
+    # Load the dataset from the CSV file
+    data = pd.read_csv(path_to_file)
+
+    # Getting the data of country codes and years
+    # Fetched the country codes from the CSV
+    country_codes = data['Country Name']
+    # The columns of years start from the 3rd column.
+    years = data.columns[1:]
+
+    # Create a line plot with multiple lines based on the number of countries
+    plt.figure(figsize=(10, 6))
+    for idx in range(len(country_codes)):
+        # The data of years starts from column 3.
+        plt.plot(years, data.iloc[idx, 1:], label=country_codes[idx])
+ 
+    # Adding plot title and axis labels to append to the graphs.
+    plt.title('CO2 Emissions from 2000 to 2020')
+    plt.xlabel('Years')
+    plt.ylabel('CO2 emissions (metric tons per capita)')
+
+    # Adding legend to the map countrywide
+    plt.legend()
+
+    # Display the plot after generating
+    plt.show()
+
+    # Statistical Analysis using NumPy and SciPy
+    # Calculate mean and standard deviation
+    mean_yield = np.mean(data.iloc[:, 1:], axis=0)
+    std_yield = np.std(data.iloc[:, 1:], axis=0)
+
+    # Calculate skewness and kurtosis using SciPy's skewness and kurtosis functions
+    skewness_yield = skew(data.iloc[:, 1:], axis=1)
+    kurtosis_yield = kurtosis(data.iloc[:, 1:], axis=1)
+
+    # Print the statistical results
+    print("Mean of CO2 Emissions: ", mean_yield)
+    print("Standard Deviation of CO2 Emissions: ", std_yield)
+    print("Skewness of CO2 Emissions: ", skewness_yield)
+    print("Kurtosis of CO2 Emissions: ", kurtosis_yield)
+
+# Calling the function to generate the Line Plot
+path_to_file = 'CO2Emissions.csv'
+plot_line(path_to_file)
---
+++ HeatMap_Russia.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import skew, kurtosis
+
+def plot_heatmap(file_path, indicators):
+    """
+    Generate a correlation heatmap and display statistical results for given indicators.
+
+    Parameters:
+    - file_path (str): Path to the CSV file containing the data.
+    - indicators (list): List of indicators for correlation analysis.
+
+    Returns:
+    - None
+    """
+    
+    # Read data from CSV file into a DataFrame
+    data = pd.read_csv(file_path)
+
+    # Extract relevant columns for correlation analysis
+    indicators_data = data[indicators]
+
+    # Calculate correlation matrix
+    correlation_matrix = indicators_data.corr()
+
+    # Statistical Analysis using NumPy and SciPy
+    # Calculate mean and standard deviation for each indicator
+    mean_indicators = np.mean(indicators_data[indicators], axis=0)
+    std_indicators = np.std(indicators_data[indicators], axis=0)
+
+    # Calculate skewness and kurtosis for each indicator using SciPy's functions
+    skewness_indicators = skew(indicators_data[indicators], axis=0)
+    kurtosis_indicators = kurtosis(indicators_data[indicators], axis=0)
+
+    # Print the statistical results for each indicator
+    for i in range(len(indicators)):
+        print(f"Mean of {indicators[i]}: {mean_indicators[i]}")
+        print(f"Standard Deviation of {indicators[i]}: {std_indicators[i]}")
+        print(f"Skewness of {indicators[i]}: {skewness_indicators[i]}")
+        print(f"Kurtosis of {indicators[i]}: {kurtosis_indicators[i]}")
+        print("----------------------")
+
+    # Plot the heatmap
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
+    plt.title('Correlation Heatmap for Russia')
+    plt.show()
+
+
+file_path = 'RussiaData.csv'
+
+# Specifying the indicators of interest
+selected_indicators = ['Health Expenditure', 'Education Expenditure', 'GDP Growth', 'CO2 Emissions', 'Unemployment Rate', 'Internate Usage']
+
+# Call the function to generate the heatmap and display statistical results
+plot_heatmap(file_path, selected_indicators)
