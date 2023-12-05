c8190626cdbdd8827b2e92615e794ab43085ecff
Nethra <nethragurumallesh08@gmail.com>
2023-12-05 17:16:00 +0000

    Update BarChart_Unemp.py
    followed pep-8 guidelines and included doc strings

--- BarChart_Unemp.py
+++ BarChart_Unemp.py
@@ -8,29 +8,31 @@ def plot_unemp(data):
     Plot the unemployment rate over the years for different countries.
 
     Parameters:
-    - data (dict): Dictionary containing urbanization rate 
+    - data (dict): Dictionary containing unemployment rate 
 	data for different countries and years.
     """
 
     countries = list(data.keys())
     years = list(data[countries[0]].keys())
 
-    bar_width = 0.1  # Value to set the width of the bar
+    bar_width = 0.1  # Adjust this value to make bars thinner or thicker
 	
-     # Setting the plot size
+     # Adjust the figure size as needed
     fig, ax = plt.subplots(figsize=(10, 6))  
 
     for i, year in enumerate(years):
         values = [data[country][year] for country in countries]
         # Offset the bars for each year to avoid overlap
-        ax.bar([x + i * bar_width for x in range(len(countries))], values, width=bar_width, label=year)
+        ax.bar([x + i * bar_width for x in range(len(countries))], 
+            values, width=bar_width, label=year)
 
     ax.set_xlabel('Countries')
     ax.set_ylabel('Unemployment rate')
     ax.set_title('Unemployment Rate for Different Countries')
 
     # Center the x-ticks under each group of bars
-    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 for i in range(len(countries))])
+    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 
+      for i in range(len(countries))])
     ax.set_xticklabels(countries)
 
     # Move legend outside the plot area
@@ -39,7 +41,8 @@ def plot_unemp(data):
     plt.show()
 
     # Extract values for descriptive statistics
-    values_array = np.array([data[country][year] for year in years for country in countries])
+    values_array = np.array([data[country][year] 
+      for year in years for country in countries])
 
     # Descriptive statistics using NumPy
     print("Descriptive Statistics:")
@@ -51,7 +54,8 @@ def plot_unemp(data):
 
 # Read data from CSV
 file_path = 'Unemployment.csv'
-unemp_data = pd.read_csv(file_path, index_col='Country Name').to_dict(orient='index')
+unemp_data = pd.read_csv(file_path, index_col='Country Name') \
+                .to_dict(orient='index')
 
 # Plotting the data with a larger figure size and adjusted legend position
 plot_unemp(unemp_data)
