ae10a55a82747bbdaa27754adf06044f8aa70db9
Nethra <nethragurumallesh08@gmail.com>
2023-12-05 17:15:02 +0000

    Update InternetUsage.py
    followed pep-8 guidelines and included doc strings

--- InternetUsage.py
+++ InternetUsage.py
@@ -3,57 +3,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import kurtosis, skew
 
-def plot_line(path_to_file):
+def process_data(path_to_file):
     """
-    Generate a line plot representing Internet Usage from 2000 to 2020 for different countries.
+    Read a dataframe in World Bank format and return two dataframes:
+    one with years as columns and one with countries as columns.
 
     Parameters:
     - path_to_file (str): The path to the CSV file containing the dataset.
 
     Returns:
+    - DataFrame: Dataframe with years as columns
+    - DataFrame: Dataframe with countries as columns
+    """
+    data = pd.read_csv(path_to_file)
+    data_countries = data.set_index('Country Name')
+    data_years = data_countries.T
+    return data_years, data_countries
+
+def plot_line(data_countries):
+    """
+    Generate a line plot representing Internet Usage from 2000 to 2020 for
+    different countries.
+
+    Parameters:
+    - data_countries (DataFrame): Dataframe with countries as columns
+
+    Returns:
     None
     """
-    # Load the dataset from the CSV file
-    data = pd.read_csv(path_to_file)
-
-    # Getting the data of country codes and years
-    # Fetched the country codes from the CSV
-    country_codes = data['Country Name']
-    # The columns of years start from the 3rd column.
-    years = data.columns[1:]
-
-    # Create a line plot with multiple lines based on the number of countries
     plt.figure(figsize=(10, 6))
-    for idx in range(len(country_codes)):
-        # The data of years starts from column 3.
-        plt.plot(years, data.iloc[idx, 1:], label=country_codes[idx])
- 
-    # Adding plot title and axis labels to append to the graphs.
+    for country in data_countries.index:
+        plt.plot(data_countries.columns, 
+           data_countries.loc[country], label=country)
+
     plt.title('Internet Usage from 2000 to 2020')
     plt.xlabel('Years')
     plt.ylabel('Individuals using the Internet (% of population)')
 
-    # Adding legend to the map countrywide
     plt.legend()
-
-    # Display the plot after generating
     plt.show()
 
-    # Statistical Analysis using NumPy and SciPy
-    # Calculate mean and standard deviation
-    mean_yield = np.mean(data.iloc[:, 1:], axis=0)
-    std_yield = np.std(data.iloc[:, 1:], axis=0)
+    mean_yield = np.mean(data_countries, axis=0)
+    std_yield = np.std(data_countries, axis=0)
+    skewness_yield = skew(data_countries, axis=1)
+    kurtosis_yield = kurtosis(data_countries, axis=1)
 
-    # Calculate skewness and kurtosis using SciPy's skewness and kurtosis functions
-    skewness_yield = skew(data.iloc[:, 1:], axis=1)
-    kurtosis_yield = kurtosis(data.iloc[:, 1:], axis=1)
-
-    # Print the statistical results
     print("Mean of Internet Usage: ", mean_yield)
     print("Standard Deviation of Internet Usage: ", std_yield)
     print("Skewness of Internet Usage: ", skewness_yield)
     print("Kurtosis of Internet Usage: ", kurtosis_yield)
 
-# Calling the function to generate the Line Plot
+# Calling the function to process the data
 path_to_file = 'InternetUsage.csv'
-plot_line(path_to_file)
+data_years, data_countries = process_data(path_to_file)
+
+# Calling the function to generate the Line Plot
+plot_line(data_countries)
