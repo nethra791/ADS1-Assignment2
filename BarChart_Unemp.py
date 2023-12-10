138c4734efc0f02ed050f5d7eb2371ea37dc10c0
Nethra <nethragurumallesh08@gmail.com>
2023-12-10 22:24:29 +0000

    Update BarChart_Unemp.py
    code changes are done as per pep-8 guidelines

--- BarChart_Unemp.py
+++ BarChart_Unemp.py
@@ -1,61 +1,75 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis
 
+
+def read_file(filename):
+    
+    """
+    Reads a dataframe in World Bank format and returns two dataframes:
+    one with years as columns, and another with countries as columns.
+
+    Parameters:
+    - filename (str): Path to the CSV file.
+
+    Returns:
+    - Tuple[pd.DataFrame, pd.DataFrame]: A tuple of two dataframes.
+    """
+    df = pd.read_csv(filename, index_col='Country Name')
+    df_y = df.transpose()
+
+    # Clean the transposed dataframe
+    df_y.index = pd.to_datetime(
+        df_y.index).year
+    df_y.index.name = 'Year'
+
+    return df, df_y
+
+
 def plot_unemp(data):
+    
     """
     Plot the unemployment rate over the years for different countries.
 
     Parameters:
-    - data (dict): Dictionary containing unemployment rate 
-	data for different countries and years.
+    - data (dict): Dictionary containing unemployment rate data for 
+      different countries and years.
     """
-
     countries = list(data.keys())
     years = list(data[countries[0]].keys())
 
-    bar_width = 0.1  # Adjust this value to make bars thinner or thicker
-	
-     # Adjust the figure size as needed
-    fig, ax = plt.subplots(figsize=(10, 6))  
+    bar_width = 0.1
+    fig, ax = plt.subplots(figsize=(10, 6))
 
     for i, year in enumerate(years):
         values = [data[country][year] for country in countries]
-        # Offset the bars for each year to avoid overlap
         ax.bar([x + i * bar_width for x in range(len(countries))], 
-            values, width=bar_width, label=year)
+               values, width=bar_width, label=year)
 
     ax.set_xlabel('Countries')
     ax.set_ylabel('Unemployment rate')
     ax.set_title('Unemployment Rate for Different Countries')
-
-    # Center the x-ticks under each group of bars
     ax.set_xticks([i + (len(years) - 1) * bar_width / 2 
-      for i in range(len(countries))])
+                   for i in range(len(countries))])
     ax.set_xticklabels(countries)
-
-    # Move legend outside the plot area
     ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.show()
 
-    # Extract values for descriptive statistics
-    values_array = np.array([data[country][year] 
-      for year in years for country in countries])
+    # using statistics methods
+    val = np.array(
+        [data[country][year] for year in years for country in countries]
+    )
+    print("Statistics:")
+    print(pd.DataFrame(val).describe())
+    print("\nSkewness:", skew(val))
+    print("Kurtosis:", kurtosis(val))
 
-    # Descriptive statistics using NumPy
-    print("Descriptive Statistics:")
-    print(pd.DataFrame(values_array).describe())
+# Calling functions to create plot
+filename = 'Unemployment.csv'
+df_c, df_y = read_file(filename)
 
-    # Skewness and Kurtosis using Scipy
-    print("\nSkewness:", skew(values_array))
-    print("Kurtosis:", kurtosis(values_array))
-
-# Read data from CSV
-file_path = 'Unemployment.csv'
-unemp_data = pd.read_csv(file_path, index_col='Country Name') \
-                .to_dict(orient='index')
-
-# Plotting the data with a larger figure size and adjusted legend position
+# Converting to dictionary format for plotting function
+unemp_data = df_c.to_dict(orient='index')
 plot_unemp(unemp_data)
