bc0caf4f25bc62f4cecd8e6f705ee6f72d62c356
Nethra <nethragurumallesh08@gmail.com>
2023-12-10 22:33:26 +0000

    Update InternetUsage.py
    made code changes as per pep-8 guidelines

--- InternetUsage.py
+++ InternetUsage.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import kurtosis, skew
 
-def process_data(path_to_file):
+
+def read_data(path_to_file):
     """
     Read a dataframe in World Bank format and return two dataframes:
     one with years as columns and one with countries as columns.
@@ -16,37 +17,38 @@ def process_data(path_to_file):
     - DataFrame: Dataframe with countries as columns
     """
     data = pd.read_csv(path_to_file)
-    data_countries = data.set_index('Country Name')
-    data_years = data_countries.T
-    return data_years, data_countries
+    data_c = data.set_index('Country Name')
+    data_y = data_c.T
+    return data_y, data_c
 
-def plot_line(data_countries):
+
+def plot_line(data_c):
     """
     Generate a line plot representing Internet Usage from 2000 to 2020 for
     different countries.
 
     Parameters:
-    - data_countries (DataFrame): Dataframe with countries as columns
+    - data_c (DataFrame): Dataframe with countries as columns
 
     Returns:
     None
     """
     plt.figure(figsize=(10, 6))
-    for country in data_countries.index:
-        plt.plot(data_countries.columns, 
-           data_countries.loc[country], label=country)
+    for country in data_c.index:
+        plt.plot(data_c.columns, 
+           data_c.loc[country], label=country)
 
     plt.title('Internet Usage from 2000 to 2020')
     plt.xlabel('Years')
     plt.ylabel('Individuals using the Internet (% of population)')
-
+    # adding legend
     plt.legend()
     plt.show()
 
-    mean_yield = np.mean(data_countries, axis=0)
-    std_yield = np.std(data_countries, axis=0)
-    skewness_yield = skew(data_countries, axis=1)
-    kurtosis_yield = kurtosis(data_countries, axis=1)
+    mean_yield = np.mean(data_c, axis=0)
+    std_yield = np.std(data_c, axis=0)
+    skewness_yield = skew(data_c, axis=1)
+    kurtosis_yield = kurtosis(data_c, axis=1)
 
     print("Mean of Internet Usage: ", mean_yield)
     print("Standard Deviation of Internet Usage: ", std_yield)
@@ -55,7 +57,7 @@ def plot_line(data_countries):
 
 # Calling the function to process the data
 path_to_file = 'InternetUsage.csv'
-data_years, data_countries = process_data(path_to_file)
+data_y, data_c = read_data(path_to_file)
 
 # Calling the function to generate the Line Plot
-plot_line(data_countries)
+plot_line(data_c)
