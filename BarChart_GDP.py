92d077e2cad5e00206c233e1146072f19e92991a
Nethra <nethragurumallesh08@gmail.com>
2023-12-10 22:17:07 +0000

    Update BarChart_GDP.py
    code changes done following pep-8

--- BarChart_GDP.py
+++ BarChart_GDP.py
@@ -3,62 +3,69 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import skew, kurtosis
 
-def plot_gdp(data):
+
+def read_file(file_name):
+    
     """
-    Plot the GDP Growth rate over the years for different countries.
+    Reads a DataFrame in Worldbank format and returns two DataFrames:
+    one with years as columns, another with countries as columns.
 
     Parameters:
-    - data (dict): Dictionary containing GDP growth rate 
-	data for different countries and years.
-    """
+    - file_name (str): Filename of the CSV file to read.
 
+    Returns:
+    - Tuple of two DataFrames: (df_y, df_c)
+    """
+    df = pd.read_csv(file_name, index_col=0)
+    df_y = df.T
+    df_c = df
+
+    # Cleaning NaNs or placeholders
+    df_y = df_y.apply(pd.to_numeric, errors='coerce')
+    df_c = df_c.apply(pd.to_numeric, errors='coerce')
+
+    return df_y, df_c
+
+
+def plot_gdp(data):
+    
+    """
+    Plot the GDP Growth rate over the years for different countries.
+    """
     countries = list(data.keys())
     years = list(data[countries[0]].keys())
 
-    bar_width = 0.1  # Value to set the thickness of the bar
-	
-     # Adjust the figure size as needed
-    fig, ax = plt.subplots(figsize=(10, 6))  
+    bar_width = 0.1
+    fig, ax = plt.subplots(figsize=(10, 6))
 
     for i, year in enumerate(years):
         values = [data[country][year] for country in countries]
-        # Offset the bars for each year to avoid overlap
         ax.bar([x + i * bar_width for x in range(len(countries))],
-       values, width=bar_width, label=year)
+               values, width=bar_width, label=year)
+
     ax.set_xlabel('Countries')
     ax.set_ylabel('GDP growth (annual %)')
     ax.set_title('GDP growth Over the Years for Different Countries')
 
-    # Center the x-ticks under each group of bars
-    ax.set_xticks([
-    i + (len(years) - 1) * bar_width / 2 for i in range(len(countries))
-])
+    ax.set_xticks([i + (len(years) - 1) * bar_width / 2 
+                   for i in range(len(countries))])
     ax.set_xticklabels(countries)
-
-    # Move legend outside the plot area
     ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.show()
 
-    # Extract values for descriptive statistics
-    values_array = np.array([
-    data[country][year] for year in years for country in countries
-])
+    val_array = np.array([data[country][year] for year in years 
+                             for country in countries])
+
+    print("Statistics:")
+    print(pd.DataFrame(val_array).describe())
+    print("\nSkewness:", skew(val_array))
+    print("Kurtosis:", kurtosis(val_array))
 
 
-    # Descriptive statistics using NumPy
-    print("Descriptive Statistics:")
-    print(pd.DataFrame(values_array).describe())
-
-    # Skewness and Kurtosis using Scipy
-    print("\nSkewness:", skew(values_array))
-    print("Kurtosis:", kurtosis(values_array))
-
-# Read data from CSV
+# Calling function to create plot
 file_path = 'GDP.csv'
-gdp_data = pd.read_csv(file_path, index_col='Country Name') \
-              .to_dict(orient='index')
+df_y, df_c = read_file(file_path)
 
-
-# Plotting the data with a larger figure size and adjusted legend position
+gdp_data = df_c.to_dict(orient='index')
 plot_gdp(gdp_data)
